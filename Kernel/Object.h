5d6565bd1183a1b46d0b3b85524115b416c4101f
SayaSasamiya <35210803+SayaSasamiya@users.noreply.github.com>
2018-01-08 06:15:10 +0100

    Add files via upload

--- Kernel/Object.h
+++ Kernel/Object.h
@@ -1,1 +1,21 @@
+/*Copyright 2018 Albedo Technologies, all rights reserved
+Author: Samuel Alonso
+Contact: somethingghanging@gmail.com
+Date: 08/01/2018(dd/mm/yyyy)
+Version: 1
+Description: N/A
+*/
+#pragma once
 
+#include <Albedo Engine\Types.h>
+
+namespace AlbedoEngine
+{
+	class Object
+	{
+	protected:
+		virtual S8* ToString();
+
+		virtual S8* Type();
+	};
+}
